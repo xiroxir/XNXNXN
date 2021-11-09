bf957d5f55af88f06430ea7196d448f3693ba554
xiroxir <86355893+xiroxir@users.noreply.github.com>
2021-11-09 12:00:14 +0700

    Add files via upload

--- чат бот со списком.py
+++ чат бот со списком.py
@@ -1,6 +1,7 @@
 cpicok = []
 comand = 0
 play = 0
+pmchim = 0
 print("Чтобы начать общаться с чат-ботом напишите play")
 play = input("Введите play:")
 if play == "play":
@@ -21,11 +22,29 @@ if play == "play":
             for cpic in cpicok:
                 print(cpic)
         elif comand == 'показать элемент по номеру':
-            num3 = input('Введите число:')
+            num3 = input('Введите номер, чтобы узнать, что это в списке:')
             try:
                 print(cpicok[int(num3)-1])
             except:
                 print('Такого элемента не обнаружено в этом списке')
+        elif comand == 'редактировать список по номеру':
+            num4 = input('Введите номер, который хотите редактировать:')
+            redak1 = input('Введите слово, на которое вы хотите заменить:')
+            try:
+                cpicok[int(num4)-1] = redak1
+                print('Элемент успешно отредактирован')
+            except:
+                print('Такого элемента не обнаружено в этом списке')
+        elif comand == 'редактировать элемент списка по имени':
+            num5 = input('Введите имя элемента, который хотите отредактировать:')
+            redak2 = input('Введите имя элемента, на что вы хотите отредактировать:')
+            if num5 in cpicok:
+                for i in range (0, len(cpicok)):
+                    if list[i]==num5:
+                        list[i]=redak2
+                        print('Элемент успешно отредактирован')
+                    else:
+                        print('Упс, что-то пошло не так')
         else:
             print('Данной команды не обнаружено')
-        comand = input('Введите команду:')
+        comand = input('Введите команду:')        
