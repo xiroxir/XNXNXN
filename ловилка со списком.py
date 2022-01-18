69588af5dac2434d0563db30a1fc3c91b1734c71
xiroxir <86355893+xiroxir@users.noreply.github.com>
2022-01-18 10:46:53 +0700

    Add files via upload

--- ловилка со списком.py
+++ ловилка со списком.py
@@ -1,16 +1,25 @@
 import time
 import random
 import keyboard as kb
-yX = -1
-xX = 0
-y_bonus = -1
-x_bonus = 0
-num1 = 0
+yX1 = -1
+xX1 = 0
+yX2 = -1
+xX2 = 0
+yX3 = -1
+xX3 = 0
+y_bonus1 = -1
+x_bonus1 = 0
+y_bonus2 = -1
+x_bonus2 = 0
+y_bonus3 = -1
+x_bonus3 = 0
 start = 0
-XorD = 0
-otchet = 0
-taimer = random.randint(1,5)
+taimer1 = random.randint(1,5)
 taimer2 = random.randint(1,5)
+taimer3 = random.randint(1,5)
+taimer4 = random.randint(1,5)
+taimer5 = random.randint(1,5)
+taimer6 = random.randint(1,5)
 
 def vivod():
     for pipi in spicok:
@@ -20,37 +29,109 @@ def vivod():
     print()
 
 def padenie():
-    global XorD, yX, xX, y_bonus, x_bonus, taimer, taimer2ы
-    XorD = random.randint(0,1)
-    xX = random.randint(0,8)
-    x_bonus = random.randint(0,8)
+    taimer1 = random.randint(1,5)
+    taimer2 = random.randint(1,5)
+    taimer3 = random.randint(1,5)
+    taimer4 = random.randint(1,5)
+    taimer5 = random.randint(1,5)
+    taimer6 = random.randint(1,5)
+    yX1 = -1
+    xX1 = 0
+    yX2 = -1
+    xX2 = 0
+    yX3 = -1
+    xX3 = 0
+    y_bonus1 = -1
+    x_bonus1 = 0
+    y_bonus2 = -1
+    x_bonus2 = 0
+    y_bonus3 = -1
+    x_bonus3 = 0
+    
+    
+    xX1 = random.randint(0,8)
+    x_bonus1 = random.randint(0,8)
     while True:
         vivod()
-        if taimer==0:
-            spicok[yX][xX] = '*'
-            yX = yX+1
-            if yX == 10:
-                yX = -1
-                xX = random.randint(0,8)
-                taimer = random.randint(1,5)
+        if taimer1==0:
+            spicok[yX1][xX1] = '*'
+            yX1 = yX1+1
+            if yX1 == 10:
+                yX1 = -1
+                xX1 = random.randint(0,8)
+                taimer1 = random.randint(1,5)
             else:
-                spicok[yX][xX] = 'X'
+                spicok[yX1][xX1] = 'X'
         else:
-            taimer = taimer - 1
-            spicok[yX][xX] = '*'
-        
+            taimer1 = taimer1 - 1
+            spicok[yX1][xX1] = '*'
+            
         if taimer2==0:
-            spicok[y_bonus][x_bonus] = '*'
-            y_bonus = y_bonus + 1
-            if y_bonus == 10:
-                y_bonus = -1
-                x_bonus = random.randint(0,8)
+            spicok[yX2][xX2] = '*'
+            yX2 = yX2+1
+            if yX2 == 10:
+                yX2 = -1
+                xX2 = random.randint(0,8)
                 taimer2 = random.randint(1,5)
             else:
-                spicok[y_bonus][x_bonus] = '$'
+                spicok[yX2][xX2] = 'X'
         else:
             taimer2 = taimer2 - 1
-            spicok[y_bonus][x_bonus] = '*'
+            spicok[yX2][xX2] = '*'
+            
+        if taimer3==0:
+            spicok[yX3][xX3] = '*'
+            yX3 = yX3+1
+            if yX3 == 10:
+                yX3 = -1
+                xX3 = random.randint(0,8)
+                taimer3 = random.randint(1,5)
+            else:
+                spicok[yX3][xX3] = 'X'
+        else:
+            taimer3 = taimer3 - 1
+            spicok[yX3][xX3] = '*'
+        
+        if taimer4==0:
+            spicok[y_bonus1][x_bonus1] = '*'
+            y_bonus1 = y_bonus1 + 1
+            if y_bonus1 == 10:
+                y_bonus1 = -1
+                x_bonus1 = random.randint(0,8)
+                taimer4 = random.randint(1,5)
+            else:
+                spicok[y_bonus1][x_bonus1] = '$'
+        else:
+            taimer4 = taimer4 - 1
+            spicok[y_bonus1][x_bonus1] = '*'
+        time.sleep(1)
+        
+        if taimer5==0:
+            spicok[y_bonus2][x_bonus2] = '*'
+            y_bonus2 = y_bonus2 + 1
+            if y_bonus2 == 10:
+                y_bonus2 = -1
+                x_bonus2 = random.randint(0,8)
+                taimer5 = random.randint(1,5)
+            else:
+                spicok[y_bonus2][x_bonus2] = '$'
+        else:
+            taimer5 = taimer5 - 1
+            spicok[y_bonus2][x_bonus2] = '*'
+        time.sleep(1)
+        
+        if taimer6==0:
+            spicok[y_bonus3][x_bonus3] = '*'
+            y_bonus3 = y_bonus3 + 1
+            if y_bonus3 == 10:
+                y_bonus3 = -1
+                x_bonus3 = random.randint(0,8)
+                taimer6 = random.randint(1,5)
+            else:
+                spicok[y_bonus3][x_bonus3] = '$'
+        else:
+            taimer6 = taimer6 - 1
+            spicok[y_bonus3][x_bonus3] = '*'
         time.sleep(1)
    
 spicok = [['*','*','*','*','*','*','*','*','*'],
