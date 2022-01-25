9c6d9a672c6515ae64ddbd335b89f313fd08017c
xiroxir <86355893+xiroxir@users.noreply.github.com>
2022-01-25 11:24:15 +0700

    Add files via upload

--- ловилка со списком2.py
+++ ловилка со списком2.py
@@ -1,9 +1,12 @@
 import time
 import random
 import keyboard as kb
-
+hp = 3
+ochki = 0
 
 def vivod():
+    global hp, ochki
+    print(hp, ochki)
     for pipi in spicok:
             for pypy in pipi:
                 print(pypy + " ", end='')
@@ -24,15 +27,13 @@ kb.add_hotkey('right', right)
 
 def padenie():
     global yxO, hp, ochki
-    taimer1 = random.randint(1,5)
-    taimer2 = random.randint(1,5)
-    taimer3 = random.randint(1,5)
-    taimer4 = random.randint(1,5)
-    taimer5 = random.randint(1,5)
-    taimer6 = random.randint(1,5)
+    taimer1 = random.randint(0,5)
+    taimer2 = random.randint(0,5)
+    taimer3 = random.randint(0,5)
+    taimer4 = random.randint(0,5)
+    taimer5 = random.randint(0,5)
+    taimer6 = random.randint(0,5)
     yO = 9
-    hp = 3
-    ochki = 0
     xO = 4
     yX1 = -1
     xX1 = 0
@@ -56,11 +57,13 @@ def padenie():
         if taimer1==0:
             spicok[yX1][xX1] = '*'
             yX1 = yX1+1
+            if yX1 == yO and xX1 == xO:
+                hp = hp-1
             
             if yX1 == 10:
                 yX1 = -1
                 xX1 = random.randint(0,8)
-                taimer1 = random.randint(1,5)
+                taimer1 = random.randint(0,5)
             else:
                 spicok[yX1][xX1] = 'X'
         else:
@@ -70,10 +73,12 @@ def padenie():
         if taimer2==0:
             spicok[yX2][xX2] = '*'
             yX2 = yX2+1
+            if yX2 ==yO and xX2 == xO:
+                hp = hp-1
             if yX2 == 10:
                 yX2 = -1
                 xX2 = random.randint(0,8)
-                taimer2 = random.randint(1,5)
+                taimer2 = random.randint(0,5)
             else:
                 spicok[yX2][xX2] = 'X'
         else:
@@ -83,10 +88,12 @@ def padenie():
         if taimer3==0:
             spicok[yX3][xX3] = '*'
             yX3 = yX3+1
+            if yX3 == yO and xX3 == xO:
+                hp = hp-1
             if yX3 == 10:
                 yX3 = -1
                 xX3 = random.randint(0,8)
-                taimer3 = random.randint(1,5)
+                taimer3 = random.randint(0,5)
             else:
                 spicok[yX3][xX3] = 'X'
         else:
@@ -96,10 +103,12 @@ def padenie():
         if taimer4==0:
             spicok[y_bonus1][x_bonus1] = '*'
             y_bonus1 = y_bonus1 + 1
+            if y_bonus1== yO and x_bonus1  == xO:
+                ochki = ochki + 1
             if y_bonus1 == 10:
                 y_bonus1 = -1
                 x_bonus1 = random.randint(0,8)
-                taimer4 = random.randint(1,5)
+                taimer4 = random.randint(0,5)
             else:
                 spicok[y_bonus1][x_bonus1] = '$'
         else:
@@ -110,10 +119,12 @@ def padenie():
         if taimer5==0:
             spicok[y_bonus2][x_bonus2] = '*'
             y_bonus2 = y_bonus2 + 1
+            if y_bonus2 == yO and x_bonus2 == xO:
+                ochki = ochki + 1
             if y_bonus2 == 10:
                 y_bonus2 = -1
                 x_bonus2 = random.randint(0,8)
-                taimer5 = random.randint(1,5)
+                taimer5 = random.randint(0,5)
             else:
                 spicok[y_bonus2][x_bonus2] = '$'
         else:
@@ -124,10 +135,12 @@ def padenie():
         if taimer6==0:
             spicok[y_bonus3][x_bonus3] = '*'
             y_bonus3 = y_bonus3 + 1
+            if y_bonus3 == yO and x_bonus3 == xO:
+                ochki = ochki + 1
             if y_bonus3 == 10:
                 y_bonus3 = -1
                 x_bonus3 = random.randint(0,8)
-                taimer6 = random.randint(1,5)
+                taimer6 = random.randint(0,5)
             else:
                 spicok[y_bonus3][x_bonus3] = '$'
         else:
@@ -148,6 +161,7 @@ def padenie():
                 xO = xO-1
                 spicok[yO][xO] = 'O'
                 yxO = 0
+        spicok[yO][xO] = 'O'
             
                 
    
